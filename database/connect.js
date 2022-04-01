ed8abbaed12fa747d0dc7110b8a6d77374e19db0
Udayanamulya <76855517+Udayanamulya@users.noreply.github.com>
2022-04-01 08:11:07 +0800

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connect() {
-  mongoose.connect("mongodb+srv://Just:just@just.yjl3l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,
+  mongoose.connect("mongodb+srv://Udayana:yes@cluster0.yobvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
